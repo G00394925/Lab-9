2079034d495384fdcda54c3bd2cb0d835a748497
G00394925 <G00394925@atu.ie>
2024-10-18 14:27:20 +0000

    Task 3 Complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,23 +1,49 @@
+import { useState } from "react";
+
 const Create = () => {
     
     const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [poster, setPoster] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title);
+        const movie = {title, year, poster};
+        console.log(movie);
     }
 
     return(
         <div>
             <h3>Hello from create component</h3>
             <form onSubmit = {handleSubmit}>
-                <div>
+                <div className="form-group">
                     <label>Add Movie Title: </label>
                     <input type="text"
-                    className="form-control"
-                    value={title}
-                    onChange = {(e) => {setTitle(e.target.value)}} />
+                        className="form-control"
+                        value={title}
+                        onChange = {(e) => {setTitle(e.target.value)}}
+                        />
                 </div>
+
+                <div className="form-group">
+                    <label>Add Movie Year: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={year}
+                        onChange = {(e) => {setYear(e.target.value)}}
+                        />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Movie Poster: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={poster}
+                        onChange = {(e) => {setPoster(e.target.value)}}
+                        />
+                </div>
+ 
+ 
                 
                 <div>
                     <input type="submit" value="Add Movie"></input>
